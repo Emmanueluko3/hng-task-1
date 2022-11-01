f2d26efff2720f895817af5cd215ab6170d91c12
Emmanueluko3 <emmanueluko90@gmail.com>
2022-11-01 02:26:47 +0100

    footer and links fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Buttons from "./components/links";
 import Icons from './components/icons';
 import slack from "./img/slack.svg";
 import github from "./img/github.svg";
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -13,18 +14,21 @@ function App() {
       </div>
 
       <div>
-        <Buttons link="" id="twitter" text="Twitter Link" />
+        <Buttons link="https://twitter.com/__froshy" id="twitter" text="Twitter Link" />
         <Buttons link="https://training.zuri.team/" id="btn__zuri" text="Zuri Team" />
         <Buttons link="http://books.zuri.team" id="books" text="Zuri Books" />
         <Buttons link="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>" id="book__python" text="Python Books" />
         <Buttons link="https://background.zuri.team" id="pitch" text="Background Check for Coders" />
         <Buttons link="https://books.zuri.team/design-rules" id="books__design" text="Design Books" />
       </div>
-      <div className="d-flex justify-content-center">
-        <a href="" className="me-5 btn"><Icons image={slack} alternate="Slack" /></a>
-        <a href="" className="btn"><Icons image={github} alternate="Github" /></a>
+
+      <div className="d-flex justify-content-center my-3">
+        <a href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U049507QXLY" className="me-5 btn"><Icons image={slack} alternate="Slack" /></a>
+        <a href="https://github.com/Emmanueluko3" className="btn"><Icons image={github} alternate="Github" /></a>
       </div>
-      
+      <div className="my-5">
+        <Footer />
+      </div>
     </div>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import zuri_logo from "../img/zuri_logo.svg"
+import I4G from "../img/I4G.svg"
+import "../App.css"
+
+function Footer() {
+    return ( 
+        <div className="d-flex justify-content-around footer mx-5">
+            <a href="" className="my-2"><img src={zuri_logo} alt="Zuri Internship Logo" /></a>
+            <p className="my-2">HNG Internship 9 Frontend Task</p>
+            <a href="" className="my-2"><img src={I4G} alt="Zuri Internship Logo" /></a>
+        </div>
+     );
+}
+
+export default Footer;
