86cfa97e657feb3f7b94f5bf770649761a5b868a
Emmanueluko3 <emmanueluko90@gmail.com>
2022-11-04 02:31:53 +0100

    Everything fixed properly, works fine

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,76 +1,162 @@
 import "../App.css";
-
+import { useState, useEffect } from 'react'
 
 function Contact() {
+
+    //Form values
+    const initialState = {
+        firstname: "", 
+        lastname: "", 
+        email: "", 
+        message: "", 
+        // checkbox: ""
+    }
+    const [formValues, setFormValues] = useState(initialState);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormValues({...formValues, [name]: value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+  
+    }
+
+    useEffect(() => {
+        if(Object.keys(formErrors).length === 0 && isSubmit){
+        }
+  
+    },[formErrors]);
+    
+
+    const validate = (value) => {
+        const errors = {};
+        
+        if (!value.firstname) {
+            errors.firstname = "Please enter your first name!"
+        }
+        if (!value.lastname) {
+            errors.lastname = "Please enter your last name!"
+        }
+        if (!value.email) {
+            errors.email = "Please enter your email address!!"
+        } 
+        if (!value.message) {
+            errors.message = "Please enter a message!"
+        }else if (value.message.length < 10) {
+            errors.message = "Message must be more than 10 characters!"
+        }
+        // if (!value.checkbox) {
+        //     errors.checkbox = "Must be checked!"
+        // }
+        return errors
+    }
+
     return ( 
         <>
             <div className="text-start container py-2 custom-width">
                 <h2 className="fw-bold fs-1 mb-2">Contact Me</h2>
                 <p className="mb-4">Hi there, contact me to ask me about anything you have in mind.</p>
 
-                <form>
+                <form onSubmit={handleSubmit} action="#">
+
                     <div className="row">
                         <div className="mb-3 col-lg-6">
-                            <label for="firstName" 
-                            className="form-label fw-bold"
+                            <label 
+                                htmlFor="firstName" 
+                                className="form-label fw-bold"
                             >First name</label>
-                            <input type="text" 
-                            className="form-control" 
-                            id="first_name" 
-                            placeholder="Enter your first name" />
-                            {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                            <input 
+                                type="text" 
+                                className="form-control"
+                                name="firstname" 
+                                id="first_name" 
+                                placeholder="Enter your first name"
+                                value={formValues.firstname}
+                                onChange={handleChange}
+                            />
+                            <p className="form-text text-danger">{formErrors.firstname}</p>
                         </div>
 
                         <div className="mb-3 col-lg-6">
                             <label 
-                            for="lastName" 
-                            className="form-label fw-bold"
+                                htmlFor="lastName" 
+                                className="form-label fw-bold"
                             >Last name</label>
-                            <input type="text" 
-                            className="form-control" 
-                            id="last_name" 
-                            placeholder="Enter your last name" />
-                            {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                name="lastname" 
+                                id="last_name" 
+                                placeholder="Enter your last name"
+                                value={formValues.lastname}
+                                onChange={handleChange} 
+                            />
+                            <p className="form-text text-danger">{formErrors.lastname}</p>
                         </div>
                     </div>
                     <div className="mb-3">
                         <label 
-                        for="email" 
-                        className="form-label fw-bold"
+                            htmlFor="email" 
+                            className="form-label fw-bold"
                         >Email</label>
                         <input 
-                        type="email" 
-                        className="form-control" 
-                        id="email" 
-                        placeholder="yourname@email.com" />
-                        {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                            type="email" 
+                            className="form-control"
+                            name="email"  
+                            id="email" 
+                            placeholder="yourname@email.com" 
+                            value={formValues.email}
+                            onChange={handleChange}
+                        />
+                        <p className="form-text text-danger">{formErrors.email}</p>
                     </div>
 
-                    <div class="mb-3">
-                        <label for="textArea"
-                        className="form-label fw-bold"
+                    <div className="mb-3">
+                        <label 
+                            htmlFor="textArea"
+                            className="form-label fw-bold"
                         >Message</label>
                         <textarea 
-                        class="form-control" 
-                        style={{height: "120px"}} 
-                        placeholder="Send me a message and I'll reply you as soon as possible..." 
-                        id="message"></textarea>
+                            className="form-control" 
+                            style={{height: "120px"}} 
+                            placeholder="Send me a message and I'll reply you as soon as possible..."
+                            name="message" 
+                            id="message"
+                            value={formValues.message}
+                            onChange={handleChange}
+                        ></textarea>
+                        <p className="form-text text-danger">{formErrors.message}</p>
                     </div>
                     
                     <div className="mb-4 form-check">
                         <input 
-                        type="checkbox" 
-                        className="form-check-input" 
-                        id="checkBox" />
+                            type="checkbox" 
+                            className="form-check-input" 
+                            name="checkbox"
+                            id="checkBox"
+                            // value={formValues.checkbox} 
+                            // onChange={handleChange}
+                        />
                         <label 
-                        className="form-check-label" 
-                        for="checkBox">You agree to providing your data to __froshy who may contact you.</label>
+                            className="form-check-label" 
+                            htmlFor="checkBox"
+                        >You agree to providing your data to __froshy who may contact you.</label>
+                        <p className="form-text text-danger">{formErrors.checkbox}</p>
                     </div>
                     <button 
-                    type="submit" 
-                    className="btn btn-primary w-100" 
-                    id="btn__submit">Send message</button>
+                        type="submit" 
+                        className="btn btn-primary w-100" 
+                        id="btn__submit"
+                    >Send message</button>
                 </form>
+                {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
+                    alert("Message Sent Successfully") ) : "" } */}
             </div>
         </>
      );
